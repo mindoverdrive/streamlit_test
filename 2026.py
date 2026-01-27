7e42b1186b2c3aa6fd3730afeea4feadb45eeaa9
mindoverdrive <go.iketeru420@gmail.com>
2026-01-27 09:48:22 +0900

    一気にライブラリ変えた

--- 2026.py
+++ 2026.py
@@ -32,9 +32,9 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-        elif event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_ESCAPE:
-                running = False
+    
+    if pygame.key.get_pressed()[pygame.K_ESCAPE]:
+        running = False
 
     # カメラからフレームを取得
     success, image = cap.read()
